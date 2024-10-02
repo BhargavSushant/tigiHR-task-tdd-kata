8678df913010ecc9b528deaae6e0ee5a11a5cd66
BhargavSushant <BhargavSushant@gmail.com>
2024-10-02 12:26:00 +0530

    Added tests single operand and double operand

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -1,6 +1,19 @@
 const StringCalculator = require("../StringCalculator");
 
+// tests empty string
 test("returns 0 for an empty string", () => {
   const calculator = new StringCalculator();
   expect(calculator.add("")).toBe(0);
+});
+
+// test for single number
+test("returns the sum of one number", () => {
+  const calculator = new StringCalculator();
+  expect(calculator.add("1")).toBe(1);
+});
+
+// test for 2 input numbers
+test("returns the sum of two numbers", () => {
+  const calculator = new StringCalculator();
+  expect(calculator.add("1,2")).toBe(3);
 });
