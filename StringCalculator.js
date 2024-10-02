230208b0facbd958eb5a9a9b479283a2ccee5823
BhargavSushant <BhargavSushant@gmail.com>
2024-10-02 13:48:13 +0530

    Refractored StringCalculator to handle different delimeters

--- StringCalculator.js
+++ StringCalculator.js
@@ -3,9 +3,22 @@ class StringCalculator {
     if (numbers === "") {
       return 0;
     }
+    // removing delim
+    let delimiter = ",";
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n", 2);
+      delimiter = parts[0].slice(2);
+      numbers = parts[1];
+    }
+    // console.log(delimiter);
 
-    // handle one or more inputs
-    const inputArr = numbers.replace(/\n/g, ",").split(",").map(Number);
+    // replace \n with delimeter
+    const inputArr = numbers
+      .replace(/\n/g, delimiter)
+      .split(delimiter)
+      .map(Number);
+    // console.log(inputArr);
+
     return inputArr.reduce((sum, num) => sum + num, 0);
   }
 }
